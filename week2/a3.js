a07d55213b067715b8f445f206964548cf623f11
Casey Tseng <caseydyps@gmail.com>
2023-01-15 14:11:55 +0800

    update assignment 3 with if else method

--- week2/a3.js
+++ week2/a3.js
@@ -1,13 +1,18 @@
-function calculate(data) {
-    // your code here
-    calc= data.discount; //discount
-    result=(1-calc)*(data.products[0].price+data.products[1].price+data.products[2].price) //1-calc =1-discount, add up product 1~3's price
-    //console.log("Product1: "+(1-calc)*data.products[0].price+" Product2: "+(1-calc)*data.products[1].price+" Product3: "+(1-calc)*data.products[2].price);
-    //console.log("Total: "+ result)
-    return result;
-   
+    function calculate(data) {
+        // your code here
+        let calc= data.discount; //discount
+        let total =0;
+        for(let i=0;i<data.products.length;i++){
+            total=total+data.products[i].price;
+            
+        }
+        result=(1-calc)*(total); //1-calc =1-discount, add up product 1~3's price
+        //console.log("Product1: "+(1-calc)*data.products[0].price+" Product2: "+(1-calc)*data.products[1].price+" Product3: "+(1-calc)*data.products[2].price);
+        //console.log("Total: "+ result)
+        return result;
+    
 
-    }
+        }
     const discountedPrice = calculate({
     discount: 0.1,
     products: [
