174af6671ef8f2ab277e3dbadae13be51189f80f
Casey Tseng <caseydyps@gmail.com>
2023-01-20 20:41:53 +0800

    test git

--- week3/app.js
+++ week3/app.js
@@ -2,10 +2,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.send('Hello, My Server!')
 })
 
+app.get('/getData', (req, res) => {
+    var userId = req.query.number;
+    //res.send('Lack of Parameter')
+    
+    if(userId=='xyz'){
+        res.send('Wrong Parameter'); 
+    }else if(userId>0){
+        var real_number = 0.5*(parseInt(userId, 10))*(1 + (parseInt(userId, 10)));
+        res.send(`${real_number}`); 
+    }else{
+        res.send('Lack of Parameter'); 
+    }
+       
+    })
+
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
