4b99e9c478e4d26bed3d977f9fb719a30909c4e7
Casey Tseng <caseydyps@gmail.com>
2023-02-06 19:37:16 +0800

    revise folder

---
+++ week4/w4a3final/src/app.js
@@ -0,0 +1,106 @@
+// ./src/App.js
+
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+//Banner
+export const Banner = () => {
+  const [message, setMessage] = useState("Welcome message");
+
+  return (
+    <div className="banner">
+      <h2 onClick={() => setMessage("Have a Good Time!")}>{message}</h2>
+    </div>
+  );
+};
+//Footer(call to action button and hidden boxes)
+export const Footer = () => {
+  const [show, setShow] = useState(false);
+
+  return (
+    //outer & inner div for centering
+    <div className="outer">
+      <div className="inner" onClick={() => setShow(!show)}>
+        {"Call to Action"}
+      </div>
+      {show && <Box />}
+    </div>
+  );
+};
+
+const Title = "website Title/logo";
+const myTitleID = "logo";
+//title and Navbar
+export const Header = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  return (
+    <header className="main-header">
+      <nav>
+        <div className="title">
+          <h1 id={myTitleID}>{Title}</h1>
+        </div>
+        <div className="navbar">
+          <ul className="main-nav">
+            <a>item1</a>
+            <a>item2</a>
+            <a>item3</a>
+            <a>item4</a>
+          </ul>
+          <div className="menubar" onClick={() => setShowNav(!showNav)}>
+            <div className="menu"></div>
+            <div className="menu"></div>
+            <div className="menu"></div>
+          </div>
+          <nav
+            className="mobile_nav"
+            onClick={() => setShowNav(!showNav)}
+            style={{ display: showNav ? "block" : "none" }}
+          >
+            <button>X</button>
+            <a>item1</a>
+            <a>item2</a>
+            <a>item3</a>
+            <a>item4</a>
+          </nav>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+function Section() {
+  return (
+    <section>
+      <p>Section Title</p>
+    </section>
+  );
+}
+
+function Box() {
+  return (
+    <div className="box">
+      <div className="item-1 item">content BOX1</div>
+      <div className="item-2 item">content BOX2</div>
+      <div className="item-3 item">content BOX3</div>
+      <div className="item-4 item">content BOX4</div>
+    </div>
+  );
+}
+
+//1 div with 6 components
+const Myapp = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Section />
+      <Box />
+      <Footer />
+    </div>
+  );
+};
+//createRoot due to react v18
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<Myapp />);
+//test
---
+++ week4/w4a3final/src/index.js
@@ -0,0 +1,3 @@
+// ./src/index.js
+
+import "../public/index.css";
